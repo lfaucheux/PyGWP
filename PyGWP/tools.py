382a0adb8ebcced1b4f09c8feb39ca225097ec8c
Laurent Faucheux <laurent.faucheux@hotmail.fr>
2019-03-23 08:34:24 +0000

    Update tools.py

--- PyGWP/tools.py
+++ PyGWP/tools.py
@@ -5,18 +5,18 @@ __author__ = "Laurent Faucheux <faucheux@centre-cired.fr>"
 __all__    = ['cache']
 
 
-class cache(object):
+class Cache(object):
 
-        def __init__(self,*args,**kwargs):
-                """ Homemade cache class which aims at being inherited """
-                self._cache = {}
+    def __init__(self, *args, **kwargs):
+        """ Homemade cache class which aims at being inherited """
+        self._cache = {}
 
-	@classmethod
-	def _property(cls,meth):
-		@property
-		def __property(cls,*args,**kwargs):
-			meth_name = meth.__name__
-			if meth_name not in cls._cache:
-				cls._cache[meth_name] = meth(cls,*args,**kwargs)
-			return cls._cache[meth_name]
-		return __property
+    @classmethod
+    def _property(cls, meth):
+        @property
+        def __property(cls, *args, **kwargs):
+            meth_name = meth.__name__
+            if meth_name not in cls._cache:
+                cls._cache[meth_name] = meth(cls, *args, **kwargs)
+            return cls._cache[meth_name]
+        return __property
